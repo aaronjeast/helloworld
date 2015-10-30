34cbab3b2a8d1212bce17b27fd93d1a03235b477
Aaron East <aaron.east@ellucian.com>
2015-10-30 13:08:30 -0600

    added another printlin

--- Learning/src/Word.java
+++ Learning/src/Word.java
@@ -7,6 +7,9 @@ public class Word {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
+		
+		System.out.println("hellow2");
+		
 	}
 
 }
